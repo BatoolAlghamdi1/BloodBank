84fcfde695c42730f3fc954a8b445add61784ef0
BatoolAlghamdi1 <133688234+BatoolAlghamdi1@users.noreply.github.com>
2023-06-22 14:53:50 +0300

    Update Database.py

--- BloodBank/Database.py
+++ BloodBank/Database.py
@@ -1,33 +1,16 @@
-import mysql.connector 
-
-# Table creation
-# CREATE TABLE IF NOT EXISTS User (
-#     id INT PRIMARY KEY,
-#     name VARCHAR(50) NOT NULL,
-#     BloodType VARCHAR(50) NOT NULL
+import MySQLdb
 
 def insertUser( name, BloodType):
-    con = mysql.connector.connect(
-        host="rm-l4vy0h5r7t2sbvu48vo.mysql.me-central-1.rds.aliyuncs.com",
-        port = 3306,
-        user="batool",
-        password="batool@1997",
-        db="bloodbankdb"
-    )
-    cur = con.cursor()
-    try:
-        cur.execute(
-            "INSERT INTO User (name,BloodType) VALUES (%s,%s)",
-            (name,BloodType)
-        )
-        con.commit()
-        cur.close()
-        con.close()
-        return True
-    except mysql.Error as err:
-        print(err)
-        cur.close()
-        con.close()
-        return False
+  dbconnect = MySQLdb.connect("rm-l4vy0h5r7t2sbvu48vo.mysql.me-central-1.rds.aliyuncs.com", "batool", "batool@1997", "bloodbankdb")
 
+  cursor = dbconnect.cursor()
+  # cursor.execute("SELECT VERSION()")
+  cursor.execute(
+    "INSERT INTO User (name,BloodType) VALUES (%s,%s)",
+              (name,BloodType)
+  )
+  dbconnect.commit()
+  cursor.close()
 
+  dbconnect.close()
+  return True
